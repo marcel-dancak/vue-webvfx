3596ab54c4f2f0d7d250c5424a7cce622bba01d9
Marcel Dancak <dancakm@gmail.com>
2019-11-18 07:44:40 +0100

    Conditional import of main app component

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
-import App from './App.vue'
-import DevApp from './DevApp.vue'
 import Icon from './components/Icon.vue'
 
 // Content component
 import Scene from './Scene.vue'
+
+// const App = window.webvfx ? require('./App.vue').default : require('./DevApp.vue').default
+const App = process.env.NODE_ENV === 'development'
+  ? require('./DevApp.vue').default
+  : require('./App.vue').default
 
 Vue.config.productionTip = false
 Vue.component('icon', Icon)
@@ -16,7 +19,6 @@ new Vue({
         default: props => h(Scene, { props })
       }
     }
-    const targetComp = window.webvfx ? App : DevApp
-    return h(targetComp, params)
+    return h(App, params)
   }
 }).$mount('#app')
